0c6bc38b9443be295b46f1851c007e87d9a72a50
Seth Miller <seth@flexperception.com>
2022-11-06 11:28:09 -0500

    Simplify output

--- src/render.php
+++ src/render.php
@@ -1,12 +1,5 @@
 <p <?php echo get_block_wrapper_attributes(); ?>>
 
 	<?php $dateFormat = get_option('date_format'); ?>
-
-	<h2>Date format: </h2>
-	<?php echo $date; ?>
-
-	<h3>Date</h3>
-	<?php echo date($dateFormat); ?>
-
-	<?php esc_html_e( 'Simple Date Block – hello from a dynamic block!', 'fp-simple-date-block' ); ?>
+	<?php echo "Today is " . date($dateFormat); ?>
 </p>
