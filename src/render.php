8573d42a18fbe9f0aa53f9fa55421a2fbad9c77d
Seth Miller <seth@flexperception.com>
2022-11-06 10:15:12 -0500

    Sample date rendering using WP date format

--- src/render.php
+++ src/render.php
@@ -1,3 +1,12 @@
 <p <?php echo get_block_wrapper_attributes(); ?>>
+
+	<?php $dateFormat = get_option('date_format'); ?>
+
+	<h2>Date format: </h2>
+	<?php echo $date; ?>
+
+	<h3>Date</h3>
+	<?php echo date($dateFormat); ?>
+
 	<?php esc_html_e( 'Simple Date Block – hello from a dynamic block!', 'fp-simple-date-block' ); ?>
 </p>
